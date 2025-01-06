d7431f4b51196d62c1ff29138a596aaaf23a462e
Nnelo0 <broudicelouann@gmail.com>
2025-01-06 10:24:56 +0100

    Add files via upload

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebroudic <ebroudic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 17:53:43 by nnelo             #+#    #+#             */
+/*   Updated: 2025/01/06 10:12:18 by ebroudic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft/libft.h"
+# include "libft/printf/ft_printf.h"
+# include <sys/wait.h>
+
+
+#endif
