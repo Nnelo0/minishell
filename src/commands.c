ce6b574b34e73684601e02fc3283525b956481b4
Elouann Broudic <ebroudic@c2r10p2.42lehavre.fr>
2025-01-07 09:34:44 +0100

    norm

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: ebroudic <ebroudic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:01:33 by ebroudic          #+#    #+#             */
-/*   Updated: 2025/01/07 09:22:43 by ebroudic         ###   ########.fr       */
+/*   Updated: 2025/01/07 09:34:31 by ebroudic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ int	ft_exit(char *input)
 	free(input);
 	return (0);
 }
-int		ft_quotes(char *input)
+
+int	ft_quotes(char *input)
 {
 	int	i;
 	int	single_open;
@@ -50,7 +51,8 @@ void	ft_echo(char *input)
 {
 	int	i;
 
-	if (input[5] == '-' && input[6] == 'n' && (input[7] == ' ' || input[7] == '\0'))
+	if (input[5] == '-' && input[6] == 'n'
+		&& (input[7] == ' ' || input[7] == '\0'))
 	{
 		i = 8;
 		while (input[i])
@@ -64,12 +66,12 @@ void	ft_echo(char *input)
 	}
 	i = 5;
 	while (input[i])
-		{
-			while (input[i] == 34 || input[i] == 39)
-				i++;
-			write(1, &input[i], 1);
+	{
+		while (input[i] == 34 || input[i] == 39)
 			i++;
-		}
+		write(1, &input[i], 1);
+		i++;
+	}
 	ft_printf("\n");
 }
 
@@ -77,11 +79,13 @@ int	commands(char *input)
 {
 	if (!ft_quotes(input))
 		ft_printf("open quote\n");
-	if (ft_strncmp(input, "exit", 4) == 0 && (input[4] == ' ' || input[4] == '\0'))
-			return(ft_exit(input));
+	if (ft_strncmp(input, "exit", 4) == 0
+		&& (input[4] == ' ' || input[4] == '\0'))
+		return (ft_exit(input));
 	if (ft_strcmp(input, "yes") == 0)
 		ft_yes();
-	if (ft_strncmp(input, "echo", 4) == 0 && (input[4] == ' ' || input[4] == '\0'))
+	if (ft_strncmp(input, "echo", 4) == 0
+		&& (input[4] == ' ' || input[4] == '\0'))
 		ft_echo(input);
 	return (1);
 }
