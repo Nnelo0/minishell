89d26e0a04d617d2eabe7538ebaf692322fa9fc3
Elouann Broudic <ebroudic@c2r10p2.42lehavre.fr>
2025-01-07 08:52:19 +0100

    save

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: ebroudic <ebroudic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:01:33 by ebroudic          #+#    #+#             */
-/*   Updated: 2025/01/06 17:42:40 by ebroudic         ###   ########.fr       */
+/*   Updated: 2025/01/07 08:52:08 by ebroudic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_exit(char *input)
 	free(input);
 	return (0);
 }
-void	ft_echo(char *input)
+int		ft_quotes(char *input)
 {
 	int	i;
 	int	j;
@@ -36,22 +36,59 @@ void	ft_echo(char *input)
 			j = i;
 			while (input[j])
 			{
-				if ()
+				if (input[i] == 34)
+					return (1);
+				j++;
 			}
+			return (0);
 		}
+		if (input[i] == 39)
+		{
+			j = i;
+			while (input[j])
+			{
+				if (input[i] == 39)
+					return (1);
+				j++;
+			}
+			return (0);
+		}
+		i++;
 	}
+	return (1);
+}
+
+void	ft_echo(char *input)
+{
+	int	i;
+
 	if (input[5] == '-' && input[6] == 'n' && (input[7] == ' ' || input[7] == '\0'))
 	{
-		ft_putstr_fd(input + 8, 0);
+		i = 8;
+		while (input[i])
+		{
+			while (input[i] == 34 || input[i] == 39)
+				i++;
+			write(1, &input[i], 1);
+			i++;
+		}
 		return ;
 	}
-	ft_putstr_fd(input + 5, 0);
+	i = 5;
+	while (input[i])
+		{
+			while (input[i] == 34 || input[i] == 39)
+				i++;
+			write(1, &input[i], 1);
+			i++;
+		}
 	ft_printf("\n");
-
 }
 
 int	commands(char *input)
 {
+	if (!ft_quotes(input))
+		printf
 	if (ft_strncmp(input, "exit", 4) == 0 && (input[4] == ' ' || input[4] == '\0'))
 			return(ft_exit(input));
 	if (ft_strcmp(input, "yes") == 0)
