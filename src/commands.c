5c2607cecb6b9f1bf6adfb4bdc60c6bfb7b4e77b
Cyprien Le Berre <cle-berr@c2r9p1.42lehavre.fr>
2025-01-07 14:08:39 +0100

    autopush

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: cle-berr <cle-berr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:01:33 by ebroudic          #+#    #+#             */
-/*   Updated: 2025/01/07 13:59:16 by cle-berr         ###   ########.fr       */
+/*   Updated: 2025/01/07 14:06:39 by cle-berr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,15 +99,22 @@ void	ft_echo(char *input)
 
 int	commands(char *input, char **envp, t_shell *shell)
 {
+	while (*input && (*input == ' ' || *input == '\t'))
+		input++;
+	if (*input == '\0')
+		return (1);
 	shell->args = ft_split(input, ' ');
 	if (!ft_quotes(input))
-		ft_printf("open quote\n");
+		return (ft_printf("open quote\n"));
 	if (ft_strncmp(input, "exit", 4) == 0
 		&& (input[4] == ' ' || input[4] == '\0'))
 		return (ft_exit(input));
 	if (ft_strncmp(input, "echo", 4) == 0
 		&& (input[4] == ' ' || input[4] == '\0'))
+	{
 		ft_echo(input);
+		return (1);
+	}
 	if (ft_strncmp(input, "cd", 2) == 0
 		&& (input[2] == ' ' || input[2] == '\0'))
 	{
