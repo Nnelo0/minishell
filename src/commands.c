8719a291cf9349902615ec73a25bbfbb2e8515ef
Elouann Broudic <ebroudic@c2r10p2.42lehavre.fr>
2025-01-07 12:49:01 +0100

    save

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: ebroudic <ebroudic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:01:33 by ebroudic          #+#    #+#             */
-/*   Updated: 2025/01/07 12:28:43 by ebroudic         ###   ########.fr       */
+/*   Updated: 2025/01/07 12:48:37 by ebroudic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,15 +71,22 @@ void	ft_echo(char *input)
 
 int	commands(char *input, char **envp, t_shell *shell)
 {
+	while (*input  && (*input == ' ' || *input == '\t'))
+		input++;
+	if (*input == '\0')
+		return (1);
 	shell->args = ft_split(input, ' ');
 	if (!ft_quotes(input))
-		ft_printf("open quote\n");
+		return(ft_printf("open quote\n"));
 	if (ft_strncmp(input, "exit", 4) == 0
 		&& (input[4] == ' ' || input[4] == '\0'))
 		return (ft_exit(input));
 	if (ft_strncmp(input, "echo", 4) == 0
 		&& (input[4] == ' ' || input[4] == '\0'))
+	{
 		ft_echo(input);
+		return (1);
+	}
 	ft_shell(input, envp, shell);
 	return (1);
 }
